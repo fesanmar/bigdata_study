579cc1b84770c6bb8e5e1c6e499fafd83633b139
Felipe <fesanmar@gmail.com>
2020-04-12 01:13:18 +0200

    Arregla el envío del mail del formulario de contacto

--- enviar.php
+++ enviar.php
@@ -7,24 +7,22 @@ $varmail=$_POST['email'];
 $varmensaje=$_POST['mensaje'];
 
 
-$header='From:'.$varmail."\r\n";
+$header="From:bigdatastudium@gmail.com\r\n";
 $header.="X-Mailer:PHP/".phpversion()."\r\n";
 $header.="Mime-Version:1.0";
 $header.="Content-Type:text/plain";
 
-$mensaje="Has recibido un mensaje enviado por".$varnombre."\r\n";
-$mensaje.="Su e-mail de contacto es:".$varmail."\r\n";
+$mensaje="Has recibido un mensaje enviado por: ".$varnombre."\r\n";
+$mensaje.="Su e-mail de contacto es: ".$varmail."\r\n";
 $mensaje.="Contenido: \r\n".$varmensaje."\r\n";
 
 $para='bigdatastudium@gmail.com';
 $asunto='Formulario web:'.$varasunto;
 
-mail($para, $asunto, utf8_decode($mensaje),$header);
-
-
-
+if(mail($para, $asunto, utf8_decode($mensaje),$header))
+{
+    header("Location: contacto_exito.html");
+} else {
+    header("Location: contacto_error.html");
+}
 ?>
-<script language="javascript">
-alert ("Tu mensaje se ha enviado correctamente.");
-location.href="contacto_exito.html";
-</script>
